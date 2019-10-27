bd16660ff8812c40e96e6f15d420e9e7fc76625c
partha04patel <parth04patel@gmail.com>
2019-10-26 22:27:47 -0400

    Attempting for spotify client credential auth

--- server/server.js
+++ server/server.js
@@ -13,6 +13,31 @@ const session = require("express-session");
 
 const SESSION_SECRET = config.SESSION_SECRET;
 
+
+var SpotifyWebApi = require('spotify-web-api-node');
+var spotifyApi = new SpotifyWebApi({
+	clientId: '9c4e8aa5d56847059632fb4d8323ccd7',
+	clientSecret: '9f2ddb790efa4402997be72366ef6c7f'
+  });
+  
+  // Retrieve an access token
+  spotifyApi.clientCredentialsGrant().then(
+	function(data) {
+	  console.log('The access token expires in ' + data.body['expires_in']);
+	  console.log('The access token is ' + data.body['access_token']);
+  
+	  // Save the access token so that it's used in future calls
+	  spotifyApi.setAccessToken(data.body['access_token']);
+	},
+	function(err) {
+	  console.log(
+		'Something went wrong when retrieving an access token',
+		err.message
+	  );
+	}
+  );
+
+
 app.use(
 	session({
 		secret: SESSION_SECRET,
